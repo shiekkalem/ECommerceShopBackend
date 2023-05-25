0f401c0668408714fe96c838c7cd0d9ba48e7be2
kaleem sheik <shiekkaleem96@gmail.com>
2023-05-25 18:38:03 +0530

    excluding apis from authentications

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,10 +1,18 @@
 const expressJwt = require('express-jwt')
 const JWT_SECRET = process.env.JWT_SECRET
-
+const API_PATH = process.env.JWT_SECRET
 function authJwt() {
     return expressJwt({
         secret: JWT_SECRET,
         algorithms: ['HS256'],
+    }).unless({
+        path: [
+            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+
+            `${API_PATH}users/register`,
+            `${API_PATH}users/login`,
+        ],
     })
 }
 module.exports = authJwt()
